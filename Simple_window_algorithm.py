6f99cf4cb1e351c91224c54946077481b5fdeb77
oyewunmio <oluwaseyioyewunmi99@gmail.com>
2021-05-04 08:19:37 -0700

    commit2

--- Simple_window_algorithm.py
+++ Simple_window_algorithm.py
@@ -9,11 +9,16 @@ size_array = len(array)
 allowed_comb = size_array - K # this variable represents the possible index the window can be slided to for it to accumalate the window frame
 Max_sum = 0
 
-
+#approach 1
 for i in range(allowed_comb + 1):
     Window_sum = 0
     for j in range(K):
         Window_sum  = Window_sum + array[i + j]
-        print('{} + {}'.format( Window_sum ,array[i + j]))
+        #print('{} + {}'.format( Window_sum ,array[i + j]))
     Max_sum = max(Window_sum, Max_sum)
 print(Max_sum)
+
+#approach 2
+for i in range(allowed_comb + 1):
+    summer = array[i:i+allowed_comb]
+    print('{} equals {}'.format(summer,sum(summer)))
